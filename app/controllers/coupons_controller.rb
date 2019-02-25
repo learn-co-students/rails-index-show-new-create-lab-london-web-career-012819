662e1ee6905ae530f6742b27de98e0d778b7058f
Alexander Dubois <sander.dubois@outlook.com>
2019-02-25 10:40:14 +0000

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class CouponsController < ApplicationController
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def new
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def create
+    @coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
+    session[:coupon] = @coupon
+    redirect_to coupon_path(@coupon)
+  end
+end
